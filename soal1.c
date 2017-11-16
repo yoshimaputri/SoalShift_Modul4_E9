71b97d53e81ec0d2a052298bf183eaf6978deef8
jharisonn <john.harison99@gmail.com>
2017-11-16 23:57:29 +0700

    change soal1.c

--- soal1.c
+++ soal1.c
@@ -9,22 +9,14 @@
 #include <sys/time.h>
 #include <sys/stat.h>
 
-static const char *dirpath = "/home/yoshi/SoalShift_Modul4_E9/Documents";
+static const char *dirpath = "/home/jharisonn/Documents";
 char str[3][5] = {".pdf",".doc",".txt"};
-struct stat st = {0};
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
   int res;
   char fpath[1000];
   char newFile[100];
-  printf("path   : %s, len: %d\n", path, strlen(path));
-  if (strcmp(path, "/") != 0) {
-    memcpy(newFile, path, strlen(path) - 4);
-    newFile[strlen(path) - 4] = '\0';
-  } else {
-    memcpy(newFile, path, strlen(path));
-  }
-  printf("newFile: %s\n", newFile);
+  //printf("newFile: %s\n", newFile);
   sprintf(fpath,"%s%s",dirpath, newFile);
   res = lstat(fpath, stbuf);
 
@@ -33,42 +25,9 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 
   return 0;
 }
-static int xmp_open(const char *path, int flags)
-{
-    int res;
-    int counter=0;
-    char fpath[1000];
-    char path2[1000];
-    char path3[1000];
-    sprintf(fpath,"%s%s", dirpath, path);
-    sprintf(path2,"%s%s","/home/yoshi/SoalShift_Modul4_E9/Documents",path);
-    int i;
-    for(i=0;i<strlen(path);i++)
-    {
-        if(path[i]=='/')
-        {
-            counter++;
-        }
-    }
 
-    if(counter==2)
-    {
-        sprintf(path3,"%s%s","chmod 444 ",path2);
-        system(path3);
-        char temp[]="zenity --info --text=\"";
-                strcat(temp,"File Tidak Bisa Dibuka");
-                strcat(temp,"\"");
-                system(temp);
-    res = open(fpath, flags);
-    if(res == -1)
-        return -errno;
-    }
-
-    close(res);
-    return 0;
-}
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
- 	      off_t offset, struct fuse_file_info *fi)
+        off_t offset, struct fuse_file_info *fi)
 {
  char fpath[1000];
   if(strcmp(path,"/") == 0)
@@ -91,9 +50,14 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
   while ((de = readdir(dp)) != NULL) {
     char *newName;
-    int flag = 0;
     newName = de->d_name;
-    
+    // for(int i=0;i<3;i++){
+    //   if(strcmp(newName+strlen(newName) - 4 , str[i]) == 0){
+    //     printf("Terjadi kesalahan! File berisi konten berbahaya");
+    //     flag = 1;
+    //     break;
+    //   }
+    // }
     //newName = strcat(de->d_name, ".bak");
     struct stat st;
     memset(&st, 0, sizeof(st));
@@ -108,7 +72,7 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 }
 
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
- 	   struct fuse_file_info *fi)
+     struct fuse_file_info *fi)
 {
   char fpath[1000];
   char newFile[100];
@@ -126,23 +90,37 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
   }
   int res = 0;
   int fd = 0 ;
+  int flag=0;
+  for(int i=0;i<3;i++){
+    if(strcmp(fpath+strlen(fpath)-4,str[i])==0){
+      flag=1;
+      char ch,source[1000],hasil[1000],command[1000];
+      sprintf(source,"%s",fpath);
+      sprintf(hasil,"%s.ditandai",fpath);
+      int tuj=rename(source,hasil);
+      sprintf(command,"chmod 000 %s.ditandai",fpath);
+      system(command);
+      system("zenity --error --text=\"Terjadi Kesalahan! File berisi konten berbahaya.\n\" --title =\"Warning!\"");
+      break;
+    }
+  }
+  if(flag==0){
+     (void) fi;
+     fd = open(fpath, O_RDONLY);
+     if (fd == -1)
+      return -errno;
 
-  (void) fi;
-  fd = open(fpath, O_RDONLY);
-  if (fd == -1)
-    return -errno;
+     res = pread(fd, buf, size, offset);
+     if (res == -1)
+      res = -errno;
 
-  res = pread(fd, buf, size, offset);
-  if (res == -1)
-    res = -errno;
-
-  close(fd);
-  return res;
+     close(fd);
+     return res;
+  }
 }
 
 static struct fuse_operations xmp_oper = {
   .getattr  = xmp_getattr,
-  .open   = xmp_open,
   .readdir  = xmp_readdir,
   .read   = xmp_read,
 };
